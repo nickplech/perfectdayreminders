853349dad1f77619784a3b8426821a446d67c99c
nick plechas <nickplechas@gmail.com>
2018-09-06 10:21:30 -0700

    updated

--- js/phone.js
+++ js/phone.js
@@ -1,11 +1,19 @@
 var rellax = new Rellax('.rellax');
 
 
-$('#111').click(function() {
-  $('.med-texts').toggle('slow', function() {
-    // Animation complete.
-  });
+const medical = document.querySelectorAll('med-text1, med-text2, med-text3');
+const realEstate = document.querySelectorAll('home-text1, home-text2, home-text3');
+const fitness = document.querySelectorAll('fitness-text1, fitness-text2, fitness-text3');
+const spa = document.querySelectorAll('spa-text1, spa-text2, spa-text3');
+
+const	phoneBtn = document.querySelector('.option-group');
+phoneBtn.addEventListener(click, function replaceText() {
+	if(this.classList.contains(realEstate)) {
+		[]
+	}
 });
+
+
 
 
 
