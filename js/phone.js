3afb4444ac10c51231971482fc2082e4f182089d
nick plechas <nickplechas@gmail.com>
2018-09-14 03:02:07 -0700

    buttons

--- js/phone.js
+++ js/phone.js
@@ -1,17 +1,28 @@
-var rellax = new Rellax('.rellax');
 
+const phoneBtn = document.querySelector(".pho-btn-square");
 
-const medical = document.querySelectorAll('med-text1, med-text2, med-text3');
-const realEstate = document.querySelectorAll('home-text1, home-text2, home-text3');
-const fitness = document.querySelectorAll('fitness-text1, fitness-text2, fitness-text3');
-const spa = document.querySelectorAll('spa-text1, spa-text2, spa-text3');
+phoneBtn.onclick = function(event){
 
-const	phoneBtn = document.querySelector('.option-group');
-phoneBtn.addEventListener(click, function replaceText() {
-	if(this.classList.contains(realEstate)) {
-		[]
+	if (document.getElementById('doc')) {
+		document.getElementById("1").src = "../PerfectDayReminders/img/docrem.svg";
+		document.getElementById("2").src = "../PerfectDayReminders/img/confirm.svg";
+		document.getElementById("3").src = "../PerfectDayReminders/img/confirmreminder.svg";
+	} else if (document.getElementById('fit')) {
+		document.getElementById("1").src = "../PerfectDayReminders/img/drplechas.jpg";
+		document.getElementById("2").src = "../PerfectDayReminders/img/confirm.svg";
+		document.getElementById("3").src = "../PerfectDayReminders/img/confirmreminder.svg";
+	} else if (document.getElementById('house')) {
+		document.getElementById("1").src = "../PerfectDayReminders/img/docrem.svg";
+		document.getElementById("2").src = "../PerfectDayReminders/img/confirm.svg";
+		document.getElementById("3").src = "../PerfectDayReminders/img/confirmreminder.svg";
+	} else if (document.getElementById('spa')) {
+		document.getElementById("1").src = "../PerfectDayReminders/img/docrem.svg";
+		document.getElementById("2").src = "../PerfectDayReminders/img/confirm.svg";
+		document.getElementById("3").src = "../PerfectDayReminders/img/confirmreminder.svg";
+	} else {
+		false
 	}
-});
+};
 
 
 
@@ -20,13 +31,3 @@ phoneBtn.addEventListener(click, function replaceText() {
 
 
 
-
-// $(document).ready(function(){
-//     $("button").click(function(){
-//         var div = $("div");
-//         div.animate({height: '300px', opacity: '0.4'}, "slow");
-//         div.animate({width: '300px', opacity: '0.8'}, "slow");
-//         div.animate({height: '100px', opacity: '0.4'}, "slow");
-//         div.animate({width: '100px', opacity: '0.8'}, "slow");
-//     });
-// });
